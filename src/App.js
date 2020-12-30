16be76c993e5b6851085a7c92aebb396c4467dbe
BuildTools <CynthiaWang7979@hotmail.com>
2020-12-30 20:32:05 +0800

    testing execFile - failed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import {} from 'antd';
-import forward from './model';
+import { Button } from 'antd';
+import getResult from './api';
+import { execFile } from 'child_process';
+import { DragUpload } from './upload_component';
 import './App.css';
 
 function App() {
+  const child = execFile('node', ['--version'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+  });
   return (
     <div>
-      Hello World!
+      <DragUpload />
+      {/* <Button
+        onClick={getResult}
+      >
+        Communicate with API
+      </Button> */}
     </div>
   );
 }
--- src/model.js
+++ src/model.js
@@ -1,3 +1,3 @@
 export default function forward(image) {
-  
+  return 1
 }
